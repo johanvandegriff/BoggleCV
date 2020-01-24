164049d4c47768d8a860a1ff00f84e5537d6b06c
MichaelParaboschi <60247313+MichaelParaboschi@users.noreply.github.com>
2020-01-24 19:07:44 -0400

    Began investigating GPU computing.

---
+++ GPUTest.py
@@ -0,0 +1,10 @@
+import cv2
+ROOT_DIR="E:/Nextcloud/Projects/Boggle2.0/"
+
+VIDEO_DIR =ROOT_DIR+ "/Boggle-Videos/Boggle5x5/input/"
+
+IMG_DIR = ROOT_DIR+'/cascademan/categories/5x5/images/'
+
+LETTER_OUT_DIR = ROOT_DIR+"/Letters/"
+
+img=cv2.imread(IMG_DIR+)
